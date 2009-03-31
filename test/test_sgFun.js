064315d446285d741750100c0d0147a5b50df31d
filippo.pacini <filippo.pacini@7637f8a6-bbb8-11dd-becf-13071112e4e2>
2009-03-31 21:30:14 +0000

    add tests for Array.map augmentation

--- test/test_sgFun.js
+++ test/test_sgFun.js
@@ -1,20 +1,25 @@
+load("../lib/sgUtil.js");
 load("../lib/sgUnit.js");
 load("../lib/sgFun.js");
 
 function test_map() {
-  res = sgFun.map(function(x) {return 2*x;}, [1,2,3,4]);
-  return sgUnit.assertEqual(res, [2,4,6,8]);
+  var doubleF = function(x) {return 2*x;};
+  var res = sgFun.map(doubleF, [1,2,3,4]);
+  var res2 = [1,2,3,4].map(doubleF);
+  return [sgUnit.assertEqual(res, res2),
+	  sgUnit.assertEqual(res, [2,4,6,8]),
+	  sgUnit.assertEqual(res2, [2,4,6,8])];
 }
 
 function test_filter() {
-  res = sgFun.filter(function(x) {return x>2;}, [1,2,3,4]);
+  var res = sgFun.filter(function(x) {return x>2;}, [1,2,3,4]);
   return sgUnit.assertEqual(res, [3,4]);
 }
 
 function test_folds() {
-  res = sgFun.foldl(function(x, acc) {return acc+x;}, [1,2,3,4], 0);
-  res2 = sgFun.foldl(function(x, acc) {return acc-x;}, [1,2,3,4], 0);
-  res3 = sgFun.foldr(function(x, acc) {return acc-x;}, [1,2,3,4], 0);
+  var res = sgFun.foldl(function(x, acc) {return acc+x;}, [1,2,3,4], 0);
+  var res2 = sgFun.foldl(function(x, acc) {return acc-x;}, [1,2,3,4], 0);
+  var res3 = sgFun.foldr(function(x, acc) {return acc-x;}, [1,2,3,4], 0);
   return [sgUnit.assertEqual(res, 10),
 	  sgUnit.assertEqual(res2, -10),
           sgUnit.assertEqual(res3, -10)];
